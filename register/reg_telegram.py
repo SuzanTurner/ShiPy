84d44857b3681472c7961df53436c9443e6a7d14
Yadh <yadhnikawakde@gmail.com>
2025-07-24 11:08:59 +0530

    removed env from tracking

--- register/reg_telegram.py
+++ register/reg_telegram.py
@@ -1,13 +1,29 @@
 
+import json
+import os
+import requests
+
+CONFIG_PATH = os.path.expanduser("~/.shipy/telegram_config.json")
 
 def register_telegram(token: str, chat_id: str, message: str = "Registered from ShiPy 🚀"):
-    import requests
+    try:
+        os.makedirs(os.path.dirname(CONFIG_PATH), exist_ok=True)
+        with open(CONFIG_PATH, "w") as f:
+            json.dump({"token": token, "chat_id": chat_id}, f)
 
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    payload = {
-        "chat_id": chat_id,
-        "text": message
-    }
+        # Send registration message
+        url = f"https://api.telegram.org/bot{token}/sendMessage"
+        payload = {
+            "chat_id": chat_id,
+            "text": message
+        }
 
-    response = requests.post(url, data=payload)
-    return response.json()
+        response = requests.post(url, data=payload)
+        print("Registered Successfully")
+        return response.json()
+    
+    except Exception as e:
+        print(f"Could not register due to {e}")
+
+
+
--- yards/telegram.py
+++ yards/telegram.py
@@ -1,19 +1,23 @@
+from shipy.register.reg_telegram import CONFIG_PATH
 import requests
 import os
 import dotenv
+import json
 
 dotenv.load_dotenv()
-token = os.getenv("TELEGRAM_TOKEN")
+
+# token = os.getenv("TELEGRAM_TOKEN")
 
 class Telegram:
-    def __init__(self, bot_token: str):
+    def __init__(self, chat_id : str, bot_token: str):
         self.bot_token = bot_token
+        self.chat_id = chat_id
         self.base_url = f"https://api.telegram.org/bot{bot_token}"
 
-    def sail(self, chat_id: str, message: str, all : bool = False) -> dict:
+    def sail(self, message: str, all : bool = False) -> dict:
         url = f"{self.base_url}/sendMessage"
         payload = {
-            "chat_id": chat_id,
+            "chat_id": self.chat_id,
             "text": message
         }
         response = requests.post(url, data=payload)
@@ -25,7 +29,13 @@ class Telegram:
 }   
         if all:
             print(result)
-            return result
+            # return result
         else:
             print(to_print)
-            return to_print
+            # return to_print
+
+    def _load_config(self):
+        if os.path.exists(CONFIG_PATH):
+            with open(CONFIG_PATH, "r") as f:
+                return json.load(f)
+        raise Exception("Telegram config not found. Please register first.")
